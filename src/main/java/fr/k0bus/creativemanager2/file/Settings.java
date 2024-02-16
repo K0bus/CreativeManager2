15381a5ad68ae6de692b9c3faa4e665a5f89605c
K0bus <contact@k0bus.fr>
2024-02-17 00:09:26 +0100

    Add Settings class

---
+++ src/main/java/fr/k0bus/creativemanager2/file/Settings.java
@@ -0,0 +1,20 @@
+package fr.k0bus.creativemanager2.file;
+
+import fr.k0bus.config.Configuration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Settings extends Configuration {
+
+    JavaPlugin instance;
+
+    public Settings(JavaPlugin instance) {
+        super("config.yml", instance);
+        this.instance = instance;
+    }
+
+    public String getLang()
+    {
+        return getString("plugin.lang");
+    }
+    public String getTag(){return getString("plugin.tag");}
+}
