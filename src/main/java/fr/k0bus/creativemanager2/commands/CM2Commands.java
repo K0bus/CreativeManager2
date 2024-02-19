640548cdedd40be3ff69081c30f221b4474c8b6b
K0bus <contact@k0bus.fr>
2024-02-19 02:43:59 +0100

    Add command

--- src/main/java/fr/k0bus/creativemanager2/commands/CM2Commands.java
+++ src/main/java/fr/k0bus/creativemanager2/commands/CM2Commands.java
@@ -3,6 +3,7 @@ package fr.k0bus.creativemanager2.commands;
 import fr.k0bus.commands.Command;
 import fr.k0bus.creativemanager2.utils.CM2Utils;
 import fr.k0bus.creativemanager2.CreativeManager2;
+import fr.k0bus.creativemanager2.commands.cm2.CheckBlockCommand;
 import fr.k0bus.creativemanager2.commands.cm2.ReloadSubCommand;
 import fr.k0bus.creativemanager2.commands.cm2.SettingsSubCommand;
 import org.bukkit.command.CommandSender;
@@ -12,6 +13,7 @@ public class CM2Commands extends Command {
         super("cm2", "cm2.admin");
         addSubCommands(new SettingsSubCommand());
         addSubCommands(new ReloadSubCommand());
+        addSubCommands(new CheckBlockCommand());
         setUsage("/lagg <argument>");
         setDescription("All command added to LaggManager");
     }
