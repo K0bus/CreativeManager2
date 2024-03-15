b97f46b88f4b8ec3353964183b58c7166c33ba86
K0bus <contact@k0bus.fr>
2024-03-15 17:50:46 +0100

    Change setting command to open GUI

--- src/main/java/fr/k0bus/creativemanager2/commands/cm2/SettingsSubCommand.java
+++ src/main/java/fr/k0bus/creativemanager2/commands/cm2/SettingsSubCommand.java
@@ -1,6 +1,7 @@
 package fr.k0bus.creativemanager2.commands.cm2;
 
 import fr.k0bus.commands.SubCommands;
+import fr.k0bus.creativemanager2.gui.SettingGui;
 import fr.k0bus.creativemanager2.utils.CM2Utils;
 import fr.k0bus.creativemanager2.CreativeManager2;
 import org.bukkit.command.CommandSender;
@@ -13,6 +14,7 @@ public class SettingsSubCommand extends SubCommands {
 
     @Override
     public void run(CommandSender sender, String[] args) {
-        CM2Utils.sendRawMessage(sender, CreativeManager2.API.TAG + " &6Settings GUI not implemented yet.");
+        if(sender instanceof Player p)
+            new SettingGui(CreativeManager2.API.getInstance()).open(p);
     }
 }
