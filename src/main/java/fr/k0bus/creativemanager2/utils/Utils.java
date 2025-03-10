6a740b5a6067c4e2bb2e41780a19596035f67202
K0bus <contact@k0bus.fr>
2025-03-10 16:45:40 +0100

    Check if PlaceHolderAPI is laoded

--- src/main/java/fr/k0bus/creativemanager2/utils/Utils.java
+++ src/main/java/fr/k0bus/creativemanager2/utils/Utils.java
@@ -1,16 +1,14 @@
 package fr.k0bus.creativemanager2.utils;
 
 import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 
 public class Utils {
     public static String PAPIParse(String str, OfflinePlayer player)
     {
-        try {
+        if(Bukkit.getServer().getPluginManager().isPluginEnabled("PlaceholderAPI"))
             return PlaceholderAPI.setPlaceholders(player, str);
-        }
-        catch (Exception ignored){}
-
         return str;
     }
     public static String PAPIParse(String str)
