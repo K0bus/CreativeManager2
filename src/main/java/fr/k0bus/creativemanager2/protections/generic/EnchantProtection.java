40f5de0d56a634a08dd13a0f9a04f40928245517
K0bus <contact@k0bus.fr>
2025-03-14 16:47:01 +0100

    Add EnchantProtection

---
+++ src/main/java/fr/k0bus/creativemanager2/protections/generic/EnchantProtection.java
@@ -0,0 +1,64 @@
+package fr.k0bus.creativemanager2.protections.generic;
+
+import fr.k0bus.creativemanager2.CreativeManager2;
+import fr.k0bus.creativemanager2.protections.Protection;
+import fr.k0bus.creativemanager2.utils.CM2Utils;
+import java.util.Map;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.inventory.InventoryCreativeEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class EnchantProtection extends Protection {
+    public EnchantProtection(CreativeManager2 plugin) {
+        super(plugin, Material.ENCHANTED_BOOK);
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onInventoryCreative(InventoryCreativeEvent event) {
+        if (isDisabled()) return;
+        if (event.getWhoClicked() instanceof Player player) {
+            if (hasPermission(player)) return;
+            if (!CM2Utils.isCreativePlayer(player)) return;
+            reloadInventoryLore(player);
+        }
+    }
+
+    private void reloadInventoryLore(Player player) {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                for (ItemStack is : player.getInventory().getContents()) {
+                    if (is == null) continue;
+                    checkEnchant(is);
+                }
+            }
+        }.runTaskTimer(getPlugin(), 0L, 0L);
+    }
+
+    private void checkEnchant(ItemStack is) {
+        for (Map.Entry<Enchantment, Integer> enchantmentIntegerEntry :
+                is.getEnchantments().entrySet()) {
+            if (getConfig()
+                    .getStringList("blacklist")
+                    .contains(enchantmentIntegerEntry.getKey().getName())) {
+                is.removeEnchantment(enchantmentIntegerEntry.getKey());
+                continue;
+            }
+            int maxLevel = 0;
+            if (!getConfig().getBoolean("allow-unsafe"))
+                maxLevel = enchantmentIntegerEntry.getKey().getMaxLevel();
+            else if (getConfig()
+                    .contains("custom-max." + enchantmentIntegerEntry.getKey().getName()))
+                maxLevel = getConfig()
+                        .getInt("custom-max." + enchantmentIntegerEntry.getKey().getName());
+
+            if (enchantmentIntegerEntry.getValue() > maxLevel)
+                is.addUnsafeEnchantment(enchantmentIntegerEntry.getKey(), maxLevel);
+        }
+    }
+}
