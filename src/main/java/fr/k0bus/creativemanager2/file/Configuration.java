314ffb9063d907a7269d87c74c62a3d8a950635c
K0bus <contact@k0bus.fr>
2025-03-14 17:58:13 +0100

    Fix directory creation failed if already exist

--- src/main/java/fr/k0bus/creativemanager2/file/Configuration.java
+++ src/main/java/fr/k0bus/creativemanager2/file/Configuration.java
@@ -48,7 +48,7 @@ public class Configuration {
             if (plugin.getResource(filename) != null) {
                 plugin.saveResource(filename, false);
             } else {
-                if (!this.file.getParentFile().mkdirs()) {
+                if (!this.file.getParentFile().exists() && !this.file.getParentFile().mkdirs()) {
                     CM2Logger.exception(new Exception("Can't create directory"));
                     CreativeManager2.api.disableCM2();
                     return;
