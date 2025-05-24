041018fa4c243e60b2cbb22f4c95fbbbbebb2785
K0bus <contact@k0bus.fr>
2025-05-24 17:28:21 +0200

    Add interaction cancel in container protection for ArmorStand / ItemFrame

--- src/main/java/fr/k0bus/creativemanager2/protections/generic/ContainerProtection.java
+++ src/main/java/fr/k0bus/creativemanager2/protections/generic/ContainerProtection.java
@@ -4,10 +4,15 @@ import fr.k0bus.creativemanager2.protections.Protection;
 import fr.k0bus.creativemanager2.utils.ContainerUtils;
 import org.bukkit.Material;
 import org.bukkit.block.Container;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.ItemFrame;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityInteractEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 
 public class ContainerProtection extends Protection {
     public ContainerProtection() {
@@ -31,6 +36,16 @@ public class ContainerProtection extends Protection {
         if (!Protection.isCreativePlayer(event.getPlayer())) return;
         if (event.getBlock() instanceof Container container) {
             container.getInventory().clear();
+            sendPermissionMessage(event.getPlayer());
+        }
+    }
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onInteract(PlayerInteractEntityEvent event) {
+        if (isDisabled()) return;
+        if (hasPermission(event.getPlayer())) return;
+        if (!Protection.isCreativePlayer(event.getPlayer())) return;
+        if (event.getRightClicked() instanceof ArmorStand || event.getRightClicked() instanceof ItemFrame) {
+            event.setCancelled(true);
             sendPermissionMessage(event.getPlayer());
         }
     }
