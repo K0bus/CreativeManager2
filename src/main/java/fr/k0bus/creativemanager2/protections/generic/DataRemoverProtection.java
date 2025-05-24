9e6b1e76bcde274c86f472879aec76a652236a81
K0bus <contact@k0bus.fr>
2025-05-24 16:45:46 +0200

    Add some filters in DataRemover

--- src/main/java/fr/k0bus/creativemanager2/protections/generic/DataRemoverProtection.java
+++ src/main/java/fr/k0bus/creativemanager2/protections/generic/DataRemoverProtection.java
@@ -1,10 +1,15 @@
 package fr.k0bus.creativemanager2.protections.generic;
 
 import de.tr7zw.nbtapi.NBT;
+import dev.lone.itemsadder.api.CustomStack;
 import fr.k0bus.creativemanager2.protections.Protection;
 import fr.k0bus.creativemanager2.type.ItemDataType;
+import fr.k0bus.creativemanager2.type.ListType;
 import fr.k0bus.creativemanager2.utils.ListUtils;
+import io.th0rgal.oraxen.api.OraxenItems;
+import java.util.List;
 import java.util.Map;
+import me.RockinChaos.itemjoin.api.ItemJoinAPI;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
@@ -39,6 +44,7 @@ public class DataRemoverProtection extends Protection {
             public void run() {
                 for (ItemStack is : player.getInventory().getContents()) {
                     if (is == null) continue;
+                    if (isFiltered(is)) continue;
                     if (ItemDataType.ENCHANT.isEnabled(DataRemoverProtection.this)) checkEnchant(is);
                     if (ItemDataType.POTION_EFFECT.isEnabled(DataRemoverProtection.this)) checkPotionEffect(is);
                     if (ItemDataType.ITEM_FLAG.isEnabled(DataRemoverProtection.this)) checkItemFlag(is);
@@ -47,6 +53,57 @@ public class DataRemoverProtection extends Protection {
                 }
             }
         }.runTaskLaterAsynchronously(getPlugin(), 1L);
+    }
+
+    private List<String> getFilterList(String tag) {
+        return getConfig().getStringList("filter.list").stream()
+                .filter(s -> s.startsWith(tag))
+                .map(s -> s.substring(tag.length())) // Retire "ia:" (3 caractères)
+                .toList();
+    }
+    private ListType getFilterType() {
+        return ListType.fromString(getConfig().getString("filter.type"));
+    }
+
+    private boolean isFiltered(ItemStack itemStack) {
+        if (isMaterialFiltered(itemStack)) return true;
+        if (isIAFiltered(itemStack)) return true;
+        if (isOraxenFiltered(itemStack)) return true;
+        if (isItemJoinFiltered(itemStack)) return true;
+        return false;
+    }
+
+    private boolean isMaterialFiltered(ItemStack itemStack) {
+        return ListUtils.inList(
+                itemStack.getType().name(),
+                getFilterList("material:"),
+                getFilterType());
+    }
+
+    private boolean isIAFiltered(ItemStack itemStack) {
+        CustomStack customStack = CustomStack.byItemStack(itemStack);
+        if (customStack == null) return false;
+        return ListUtils.inList(
+                customStack.getId(),
+                getFilterList("ia:"),
+                getFilterType());
+    }
+    private boolean isOraxenFiltered(ItemStack itemStack) {
+        String oraxenItemsID = OraxenItems.getIdByItem(itemStack);
+        if (oraxenItemsID == null) return false;
+        return ListUtils.inList(
+                oraxenItemsID,
+                getFilterList("ox:"),
+                getFilterType());
+    }
+    private boolean isItemJoinFiltered(ItemStack itemStack) {
+        ItemJoinAPI itemAPI = new ItemJoinAPI();
+        String itemJoinID = itemAPI.getNode(itemStack);
+        if (itemJoinID == null) return false;
+        return ListUtils.inList(
+                itemJoinID,
+                getFilterList("ij:"),
+                getFilterType());
     }
 
     private void checkEnchant(ItemStack itemStack) {
